74e6337a5b29ec9b2c0816522485cdb1415c8ca2
Ajithvpillai09 <ajithvkallada@gmail.com>
2023-10-10 00:18:14 +0530

    securing grpc

--- server.js
+++ server.js
@@ -3,18 +3,51 @@ const connectDB = require("./domain/connectDB/db")
 const configKeys = require("./application/config/config")
 const grpc = require("@grpc/grpc-js")
 const fs = require('fs')
+const tls = require('tls')
 
 
 connectDB();
 
-const serverCredentials = grpc.ServerCredentials.createSsl(
-    fs.readFileSync("server-cert.pem"), // Your server certificate
-    [{
-      private_key: fs.readFileSync("server-key.pem"), // Your server private key
-      cert_chain: fs.readFileSync("ca-cert.pem"), // Your CA certificate
-    }],
-    true // Check client certificate
-  );
+const caPem = fs.readFileSync('ca-cert.pem')
+
+const certPool = tls.createSecureContext({
+   ca:caPem
+})
+
+const serverCert = fs.readFileSync('server-cert.pem')
+const serverKey = fs.readFileSync('server-key.pem')
+
+const serverSecureContext = tls.createSecureContext({
+  key: serverKey,
+  cert: serverCert
+});
+
+
+const conf = tls.createServer({
+  secureContext: serverSecureContext,
+  requestCert: true,             // Require client certificate
+  rejectUnauthorized: true,     // Reject unauthorized clients (optional)
+  ca: [caPem],                   // Specify the CA certificate for client verification
+}, (cleartextStream) => {
+  // Handle incoming TLS connections
+  // `cleartextStream` is the readable and writable stream for the connection
+  console.log(cleartextStream);
+});
+
+const tlsCredentials  = grpc.credentials.createSsl()
+
+
+
+
+
+// const serverCredentials = grpc.ServerCredentials.createSsl(
+//     fs.readFileSync("server-cert.pem"), // Your server certificate
+//     [{
+//       private_key: fs.readFileSync("server-key.pem"), // Your server private key
+//       cert_chain: fs.readFileSync("ca-cert.pem"), // Your CA certificate
+//     }],
+//     true // Check client certificate
+//   );
 
 server.bindAsync(`0.0.0.0:${configKeys.PORT}`,serverCredentials,(err,port)=>{
     if(err){
